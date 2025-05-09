73ebc1f2ae690db4fbe01e065220f50928f11258
youtario <ijairerick.info1.ifc@gmail.com>
2025-05-09 15:29:26 -0300

    FEAT: base html feita #2

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,56 @@
 
 <template>
   <header>
-
+  <div class="logo">
+        <h1>IFbooks</h1>
+        <p>Apreço à leitura</p>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#">Termos</a></li>
+            <li><a href="#">Equipe</a></li>
+            <li><a href="#">Envio</a></li>
+            <li><a href="#">Devoluções</a></li>
+        </ul>
+    </nav>
+    <div class="search">
+        <input type="text" placeholder="Pesquisar">
+        <button>🔍</button>
+    </div>
+    <div class="icons">
+        <a href="#">Carrinho</a>
+        <a href="#">Favotiros</a>
+        <a href="#">Usuario</a>
+    </div>
   </header>
 
   <main>
+    <div class="inicio">
+    <p>Livro de Destaque</p>
+    <h2>Cem anos de solidão</h2>
+    <p>Neste clássico de Gabriel García Marques, conheça as fabulosas aventuras dos Buendía-Iguarán, com seus milagres, fantasias e dramas que representam famílias do mundo inteiro. Romance fundamental na história da literatura,  Cem anos de solidão apresenta uma das mais fascinantes aventuras literárias do século XX.</p>
+    <button>Acessar página do livro</button>
+    </div>
+    <div class="info">
+     <p>Frete grátis para SC</p>
+     <p>Livros recomendados</p>
+     <p>Mais vendidos</p>
+    </div>
+    <div class="lançamento">
 
+
+    </div>
   </main>
+  <footer>
+    <p>IFBooks</p>
+    <p>Contato</p>
+    <ul>
+      <li>+55 47 40045263</li>
+      <li>8h às 23h - Seg a Sex</li>
+      <li>contato@ifbooks.com</li>
+    </ul>
+    <p>&copy; Alguns direitos reservados. IFBooks 2025</p>
+  </footer>
 </template>
 
 <style scoped>
