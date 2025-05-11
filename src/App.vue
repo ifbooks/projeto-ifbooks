0fa0e9841d6d4381093e59d39dd2a5c126a6ab64
youtario <ijairerick.info1.ifc@gmail.com>
2025-05-11 10:53:41 -0300

    feat: cards de livro e melhorias visuais #6

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,71 @@
-<script setup>
+<script >
+import BookCard from './components/BookCard.vue'
 
+export default {
+  components: { BookCard },
+  data() {
+    return {
+      books: [
+        {
+          title: "Comigo na Livraria",
+          author: "Martha Medeiros",
+          price: "32,24",
+          image: '/src/assets/images/comigonalivraria.jpg',
+          synopsis: "Martha Medeiros celebra sua paixão pelos livros e a leitura como um caminho para o crescimento pessoal."
+        },
+        {
+          title: "Quincas Borba",
+          author: "Machado de Assis",
+          price: "32,24",
+          image: "/src/assets/images/quincasborba.jpg",
+          synopsis: "Quincas Borba narra a ascensão e queda de Rubião, um ingênuo professor que herda uma fortuna e acaba sendo explorado, enlouquecendo e morrendo em Barbacena."
+        },
+                {
+          title: "A livraria",
+          author: "Penelope Fitzgerald",
+          price: "13,94",
+          image: "/src/assets/images/penelope.jpg",
+          synopsis: "A Livraria narra a luta de Florence Green para abrir uma livraria em uma cidade conservadora, enfrentando forte oposição e desafios que ameaçam seu sonho."
+        },
+                {
+          title: "A Hora da Estrela",
+          author: "Clarice Lispector",
+          price: "16,84",
+          image: "/src/assets/images/ahoradaestrela.jpg",
+          synopsis: "Clarice Lispector narra a vida de Macabéa, uma jovem nordestina ingênua e solitária que busca um sentido para sua existência no Rio de Janeiro."
+        },
+                {
+          title: "O Alienista",
+          author: "Colson Whitehead",
+          price: "26,92",
+          image: "/src/assets/images/alienista.jpg",
+          synopsis: "Conta a história do médico Simão Bacamarte, que funda um hospício e começa a internar quase toda a cidade, questionando os limites da sanidade."
+        },
+                {
+          title: "Mar Morto",
+          author: "Jorge Amado",
+          price: "13,95",
+          image: "/src/assets/images/marmorto.jpg",
+          synopsis: "Jorge Amado retrata a vida dos pescadores da Bahia, focando na relação entre Guma e Lívia, marcada pelo destino trágico ligado ao mar."
+        },
+                {
+          title: "Grande sertão: varedas",
+          author: "João Guimarães Rosa",
+          price: "26,04",
+          image: "/src/assets/images/grandesertao.jpg",
+          synopsis: "Guimarães Rosa apresenta a jornada de Riobaldo, um ex-jagunço que reflete sobre sua vida, seu amor por Diadorim e os dilemas do sertão."
+        },
+                {
+          title: "Flor de Poemas",
+          author: "Cecília Meireles",
+          price: "15,81",
+          image: "/src/assets/images/flordepoemas.jpg",
+          synopsis: "Cecília Meireles reúne uma coletânea de poesias que exploram temas como amor, saudade, infância e a beleza da vida."
+        },
+      ]
+    }
+  }
+}
 </script>
 
 <template>
@@ -11,7 +77,7 @@
   </div>
     <div class="search">
         <input type="text" placeholder="Pesquisar">
-        <button><img src="/src/images/lupa.png" alt="lupa"></button>
+        <button><img src="/src/assets/images/lupa.png" alt="lupa"></button>
     </div>
     <nav>
         <ul>
@@ -23,18 +89,17 @@
     </nav>
 <div class="iconbase">
     <div class="icon">
-        <img src="/src/images/carrinho.png" alt="">
+        <img src="/src/assets/images/carrinho1.png" alt="">
     </div>
     <div class="divisor2"></div>
     <div class="icon">
-        <img src="/src/images/coraçao.png" alt="">
+        <img src="/src/assets/images/heart.png" alt="">
     </div>
     <div class="divisor2"></div>
     <div class="icon">
-        <img src="/src/images/User.png" alt="">
+        <img src="/src/assets/images/usuario.png" alt="">
     </div>
 </div>
-
   </header>
 
   <main>
@@ -48,19 +113,29 @@
       </p>
       <button>Acessar página do livro</button>
     </div>
-    <img class="imagem-livro" src="../src/images/cemanosdesolidao.jpg" alt="Livro cem anos de solidão" />
+    <img class="imagem-livro" src="../src/assets/images/cemanosdesolidao.jpg" alt="Livro cem anos de solidão" />
   </div>
     <div class="linha"></div>
     <div class="info">
-     <p><img src="/src/images/Truck.png" alt="caminhão">Frete grátis para SC</p>
+     <p><img src="/src/assets/images/Truck.png" alt="caminhão">Frete grátis para SC</p>
      <div class="divider"></div>
-     <p><img src="/src/images/estrela.png" alt="estrela">Livros recomendados</p>
+     <p><img src="/src/assets/images/estrela.png" alt="estrela">Livros recomendados</p>
      <div class="divider"></div>
-     <p><img src="/src/images/livroaberto.png" alt="livro aberto">Mais vendidos</p>
+     <p><img src="/src/assets/images/livroaberto.png" alt="livro aberto">Mais vendidos</p>
     </div>
       <div class="linha"></div>
     <div class="lançamento">
 
+   <div class="container">
+    <h1>Lançamentos</h1>
+    <div class="book-grid">
+      <BookCard
+        v-for="(book, index) in books"
+        :key="index"
+        :book="book"
+      />
+    </div>
+  </div>
 
     </div>
 
@@ -70,23 +145,23 @@
     <div class="footer-left">
       <p class="footer-logo">IFbooks</p>
       <div class="footer-social">
-        <img src="/src/images/facebook.png" alt="Facebook">
-        <img src="/src/images/instagram.png" alt="Instagram">
-        <img src="/src/images/twitter.png" alt="Twitter">
+        <img src="/src/assets/images/facebook.png" alt="Facebook">
+        <img src="/src/assets/images/instagram.png" alt="Instagram">
+        <img src="/src/assets/images/twitter.png" alt="Twitter">
       </div>
     </div>
 
     <div class="footer-right">
       <p class="footer-title">Contato</p>
       <ul class="footer-contato">
-        <li><img src="/src/images/Phone.png" alt="telefone"> +55 47 40045263</li>
-        <li><img src="/src/images/relogio.png" alt="relogio"> 8h às 23h - Seg a Sex</li>
-        <li><img src="/src/images/Mail.png" alt="email"> contato@ifbooks.com</li>
+        <li><img src="/src/assets/images/Phone.png" alt="telefone"> +55 47 40045263</li>
+        <li><img src="/src/assets/images/relogio.png" alt="relogio"> 8h às 23h - Seg a Sex</li>
+        <li><img src="/src/assets/images/Mail.png" alt="email"> contato@ifbooks.com</li>
       </ul>
       <div class="footer-payment">
-        <img src="/src/images/paypal.png" alt="PayPal">
-        <img src="/src/images/mastercard.png" alt="MasterCard">
-        <img src="/src/images/visa.png" alt="Visa">
+        <img src="/src/assets/images/paypal.png" alt="PayPal">
+        <img src="/src/assets/images/mastercard.png" alt="MasterCard">
+        <img src="/src/assets/images/visa.png" alt="Visa">
       </div>
     </div>
   </div>
@@ -96,14 +171,24 @@
 </template>
 
 <style scoped>
+.container {
+  padding: 2rem;
+}
 
+.book-grid {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1.5rem;
+}
 header {
   display: flex;
   margin: 8px 80px;
   align-items: center;
   justify-content: space-between;
   padding: 10px 20px;
+  font-family: Arial, Helvetica, sans-serif;
 }
+
 ul {
     display: flex;
     list-style: none;
@@ -157,13 +242,13 @@ ul li a {
 .divisor {
     width: 2px;
     height: 30px;
-    background-color: #27AE60;
+    background-color: #000000;
     margin: 0 15px;
 }
 
 .texto {
     font-size: 14.02px;
-    color: #27AE6099;
+    color: #620096;
 }
 .icon {
   display: flex;
@@ -184,11 +269,11 @@ ul li a {
 .divisor2 {
     width: 2px;
     height: 24px;
-    background-color: #27AE60;
+    background-color: #000000;
 }
 .linha {
     height: 2px;
-    background-color: #27AE60;
+    background-color: #000000;
     width: 100%;
 }
 .inicio {
@@ -212,29 +297,31 @@ ul li a {
 
 .livro {
   display: inline-block;
-  color: #27AE60;
+  color: #620096;
   font-size: 18px;
   font-weight: bold;
-  border: 1px solid #27AE60;
+  border: 1px solid #620096;
   padding: 5px 10px;
   border-radius: 4px;
   margin-bottom: 16px;
 }
 
 h2 {
+  font-family: Arial, Helvetica, sans-serif;
   font-size: 58px;
   font-weight: bold;
   margin-bottom: 12px;
 }
 
 p {
+  font-family: Arial, Helvetica, sans-serif;
   font-size: 20 px;
   color: #555;
   margin-bottom: 20px;
 }
 
 button {
-  background-color: #27AE60;
+  background-color: #620096;
   color: white;
   border: none;
   padding: 10px 20px;
@@ -244,7 +331,7 @@ button {
 }
 
 button:hover {
-  background-color: #219150;
+  background-color: #56008c;
 }
 .info {
   display: flex;
@@ -263,6 +350,7 @@ button:hover {
   font-weight: 600;
   font-size: 22px;
   margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
 }
 
 .info img {
@@ -278,10 +366,11 @@ button:hover {
   background-color: #937DC2;
 }
 .footer {
-  background-color: #27AE60;
+  background-color: #161817;
   color: rgb(255, 255, 255);
   padding: 40px 80px 20px;
   font-size: 14px;
+  font-family: Arial, Helvetica, sans-serif;
 }
 
 .footer-container {
@@ -379,5 +468,17 @@ button:hover {
   align-items: flex-start;
   gap: 10px;
 }
-
+.container h1 {
+  font-size: 38px;
+  font-weight: 600;
+  margin: 20px 10px 40px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
+<style>
+html, body {
+  background-color: #e9e1e1;
+  margin: 0;
+  padding: 0;
+}
 </style>
